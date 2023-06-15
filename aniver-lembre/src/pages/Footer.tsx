59551f0312ddd954f53479e99a2d670c234f5710
Claudio <claudio1419teixeira2020@gmail.com>
2023-06-15 10:49:19 -0300

    rename folder

--- aniver-lembre/src/pages/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-    return (
-        <footer className="text-center bg-footer text-white h-50 pt-16">
-            <h2 className="text-4xl">Contato</h2>
-        </footer>
-    )
-}
